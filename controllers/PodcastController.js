1039b0d3fcea8eb5b27c727777286561729fdbef
katya <katya.com>
2024-04-05 09:49:37 +0600

    aws time configured

--- controllers/PodcastController.js
+++ controllers/PodcastController.js
@@ -1,25 +1,19 @@
 const AWS = require('aws-sdk');
 const fs = require('fs');
-
+const ntpClient = require('ntp-client');
 var jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Podcast = require('../models/Podcast');
-
-
 const multer = require('multer');
-const s3 = new AWS.S3({
-    accessKeyId: process.env.ACCESS_KEY_ID,
-    secretAccessKey: process.env.SECRET_ACCESS_KEY
-});
 
+let s3; // Определение переменной s3 здесь
+const upload = multer({dest: 'uploads/'});
 
-const upload = multer({ dest: 'uploads/' });
-
+// Функция uploadPodcast
 exports.uploadPodcast = async (req, res) => {
     try {
-
-        upload.single('file')(req, res, async function (err) {
+        upload.single('file')(req, res, async function(err) {
             if (err) {
                 console.error('Error uploading file:', err);
                 return res.status(400).json({ error: 'Error uploading file' });
@@ -42,12 +36,37 @@ exports.uploadPodcast = async (req, res) => {
 
             res.status(200).json({ message: 'Uploaded podcast successfully', podcast });
         });
-    }
-    catch (error) {
+    } catch (error) {
         console.error('Error uploading podcast:', error);
         return res.status(400).json({ error: 'Error uploading podcast' });
     }
-}
+};
+
+// Получение сетевого времени и установка AWS SDK
+ntpClient.getNetworkTime("pool.ntp.org", 123, function(err, date) {
+    if (err) {
+        console.log(err)
+    } else {
+        console.log('System time synchronized:', date);
+
+        const awsTime = new Date(date); // Время вашего сервера
+        const awsOffset = awsTime.getTimezoneOffset() * 60 * 1000; // Получение смещения времени вашего сервера
+        const correctedTime = new Date(awsTime.getTime() - awsOffset); // Корректировка времени вашего сервера
+
+        console.log('Corrected time:', correctedTime);
+
+        // Настройка AWS SDK с корректированным временем
+        AWS.config.update({ correctClockSkew: true, systemClockOffset: awsOffset });
+
+        s3 = new AWS.S3({
+            accessKeyId: process.env.ACCESS_KEY_ID,
+            secretAccessKey: process.env.SECRET_ACCESS_KEY,
+            region: 'eu-central-1'
+        });
+    }
+});
+
+
 
 exports.getPodcasts = async (req, res) => {
     try {
