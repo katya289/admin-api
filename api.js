57bf193bf372f818215dd23d54db0c77c6586fce
katya <katya.com>
2024-05-11 09:16:45 +0600

    added notification fetch

--- api.js
+++ api.js
@@ -4,13 +4,14 @@ const userController = require('./controllers/UserController');
 const podcastController = require('./controllers/PodcastController');
 const likeController = require('./controllers/LikeController');
 const commentController = require('./controllers/CommentController');
+const notificationController = require('./controllers/NotificationController');
 const verifyToken = require('./middleware/authJWT');
 
 
-
+router.get('/users/get/notifications', verifyToken, notificationController.getNotification);
 router.post('/users/register', userController.registerUser);
 router.post('/users/login', userController.loginUser);
-// router.post('/users/logout', verifyToken, userController.userLogout);
+
 
 router.delete('/podcasts/comment/delete/:commId', verifyToken, commentController.deleteComment);
 router.post('/podcasts/comment/add/:podcastId', verifyToken, commentController.addComment);
@@ -28,4 +29,7 @@ router.get('/users/get', verifyToken, userController.getAuthorizedUser);
 router.get('/podcasts/get', verifyToken, podcastController.getPodcasts);
 router.get('/podcasts/:id/get', verifyToken, podcastController.getPodcastsById);
 router.get('/by-category/:category', verifyToken, podcastController.getPodcastsByCategory);
+
+
+
 module.exports = router;
--- controllers/NotificationController.js
+++ controllers/NotificationController.js
@@ -1,14 +1,19 @@
-const {Podcast, User } = require('../models/index')
+const { Podcast, User, Comment } = require('../models/index');
 
+const Op = require('sequelize').Op;
 
 
 
 
 exports.getNotification = async (req, res) => {
     try {
-        
-    }
-    catch (error) {
+        const userId = req.user.id;
 
+        const comments = await Comment.findAll({ where: { userId: { [Op.ne]: userId } } });
+
+        return res.status(200).json({ comments });
+    } catch (error) {
+        console.error('Error fetching notifications:', error);
+        res.status(500).json({ error: 'Internal server error' });
     }
 }
